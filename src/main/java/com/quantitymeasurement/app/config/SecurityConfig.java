0d46502104ae005fe8dea90d0f80fee39fab1f11
ahkharsha <ahkharsha@gmail.com>
2026-05-15 11:14:16 +0530

    Update CORS policy to allow dynamic EC2 IP

--- src/main/java/com/quantitymeasurement/app/config/SecurityConfig.java
+++ src/main/java/com/quantitymeasurement/app/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
